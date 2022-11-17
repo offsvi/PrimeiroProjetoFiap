60b36edbec6dd7daa208416733123460810ebd89
Vitor Oliveira <vitorsk807@gmail.com>
2022-11-16 23:40:51 -0300

    Atualizando código

--- decisoes_compostas.py
+++ decisoes_compostas.py
@@ -3,4 +3,4 @@ idade=int(input("Digite a idade: "))
 if idade>=65:
     print("O paciente " + nome + " POSSUI atendimento prioritário!")
 else:
-    print("O paciente " + nome + " NÃO possui atendimento prioritário!")
+    print("O paciente " + nome + " NÃO possui atendimento prioritário!!")
